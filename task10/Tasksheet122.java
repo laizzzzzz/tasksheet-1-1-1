e9a231469db6157987b2c4bf9fa46b4df1f96639
Ge|ェ <116347807+laizzzzzz@users.noreply.github.com>
2025-07-10 03:26:50 +0000

    task 10

---
+++ task10/Tasksheet122.java
@@ -0,0 +1,30 @@
+// implement the printFullName() method in the Student class.
+// task 10 solve solve
+class Student {
+    private String firstName;
+    private String lastName;
+
+    public Student(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    // ito ung method to print the full name
+    public void printFullName() {
+        System.out.println(firstName + " " + lastName);
+    }
+}
+
+public class Tasksheet122 {
+    public static void main(String[] args) {
+        Student[] students = new Student[] {
+            new Student("Morgan", "Freeman"),
+            new Student("Brad", "Pitt"),
+            new Student("Kevin", "Spacey"),
+        };
+
+        for (Student s : students) {
+            s.printFullName();
+        }
+    }
+}
