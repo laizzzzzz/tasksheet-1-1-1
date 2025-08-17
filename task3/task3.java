3e236c8da5b7bcdd4b5ce307a1623363f6c63c4d
Ge|ェ <116347807+laizzzzzz@users.noreply.github.com>
2025-08-17 12:33:01 +0000

    task 3

---
+++ task3/task3.java
@@ -0,0 +1,16 @@
+public class Task3 {
+    public static void main(String[] args) {
+        String a = "Wow!";
+        String b = "Wow";
+        String c = "notWow!";
+        String d = "Wow!";
+
+        boolean b1 = a == d;                 
+        boolean b2 = d.equals(b + "!");      
+        boolean b3 = !c.equals(a);
+
+        if (b1 && b2 && b3) {
+            System.out.println("Success!");
+        }
+    }
+}
