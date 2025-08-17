fe3924c2695741209cbee0bb983ece5ff485f5b2
Ge|ェ <116347807+laizzzzzz@users.noreply.github.com>
2025-08-17 12:09:20 +0000

    tasksheet-1-1-2

---
+++ tasksheet-1-1-2.java
@@ -0,0 +1,23 @@
+//ArithmeticOperations... same lang dun sa may task 7 na folder ko heheh
+
+public class tasksheet-1-1-2 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+
+        a += b; 
+        System.out.println("After += : " + a);
+
+        a -= b; 
+        System.out.println("After -= : " + a);
+
+        a *= b; 
+        System.out.println("After *= : " + a);
+
+        a /= b;
+        System.out.println("After /= : " + a);
+
+        a %= b; 
+        System.out.println("After %= : " + a);
+    }
+}
