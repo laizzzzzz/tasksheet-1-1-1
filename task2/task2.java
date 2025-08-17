f6d01d7af6640c33a65f1e2d0986b55f4f68e4c0
Ge|ェ <116347807+laizzzzzz@users.noreply.github.com>
2025-08-17 12:30:16 +0000

    task 2

---
+++ task2/task2.java
@@ -0,0 +1,14 @@
+public class Task2 {
+    public static void main(String[] args) {
+        byte b = 3;
+        short s = 1;
+        int i = 1;
+        float f = 2.0f;
+        char c = 'H';
+        boolean bool = true;
+
+        String output = c + "" + b + s + i + "0 w" + b + "r" + i + "d " + f + " " + bool;
+        System.out.println(output);
+    }
+}
+//concatenated into a string sila
